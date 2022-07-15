95c64c149533f44863a1add2e373f1e27ec46555
jupiterxiaoxiaoyu <s2068649@ed.ac.uk>
2022-07-15 19:02:45 +0800

    api

--- pin/pin_api.py
+++ pin/pin_api.py
@@ -1,17 +1,22 @@
 import json
 import requests
+import pandas as pd
 
-url = 'https://api.estuary.tech/pinning/pins'
+ipfs_data = pd.read_csv("C:/Users/Jupiter/Desktop/ipfs_solution/output/fetch_arc69/metadata.csv") # replace with your obtained data from step 1 
+url = 'https://api.estuary.tech/pinning/pins' # use estuary as the provider, you can replace with your choice
 
-body=json.dumps({
-    'name': '', #replace with the file name 
-    'cid': '', #replace with the cid that you want to pin
-})
+dict_data = dict(zip(ipfs_data['ipfs_url'].str[7:],ipfs_data['asset_name']))
+
 headers = {
-  'Authorization':'Bearer REPLACE_WITH_YOUR_API_KEY',
-  
+    'Authorization':'Bearer RELACE_HERE', #replace with your own API key
 }
 
-response = requests.request("POST", url, headers=headers, data=body)
+for cid, asset_name in dict_data.items():
+  body=json.dumps({
+      'name': asset_name, #replace with the file name 
+      'cid': cid, #replace with the cid that you want to pin
+  })
+  
+  response = requests.request("POST", url, headers=headers, data=body)
 
-print(response.text)
+  print(response.text)
